55984775b45e4bcd7cd7b3a9908bce9859a9d1c9
Mothukuri-Ramya <o180301@rguktong.ac.in>
2022-11-05 22:03:20 +0530

    intial commit

---
+++ Project5(todo_list)/todo_list/src/todolist.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const Todolist = ({todolist,deleteHandler}) => {
+    return (
+        <div>
+            {todolist.map((todo,index) => 
+            <div key={index}>
+                <h5>{todo} &nbsp; <button onClick={() => deleteHandler(index)}>Delete</button></h5>
+            </div>)}
+        </div>
+    )
+}
+export default Todolist
