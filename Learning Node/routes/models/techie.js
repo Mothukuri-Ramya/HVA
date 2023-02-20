97b06cd3e72eb40843962aef6b635879542367fa
Mothukuri-Ramya <o180301@rguktong.ac.in>
2023-02-20 18:18:07 +0530

    intial commit

---
+++ Learning Node/routes/models/techie.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+  
+//create geolocation schema
+const GeoSchema = new Schema({
+  type:{
+    type:String,
+    default:"point"
+  },
+  coordinates:{
+    type:[Number],
+    index:"2dsphere"
+  }
+
+});
+
+//create techie schema&model
+const TechieSchema = new Schema({
+  name:{
+    type:String,
+    required:[true,"Name field is required"]
+   },
+   id:{
+    type:Number
+   },
+   role:{
+    type:String
+   },
+   geometry:GeoSchema
+ 
+});
+const Techie = mongoose.model('techie',TechieSchema);
+module.exports = Techie; 
